014171671dd4f57fc08d77b7d16a86f1b9ec7f37
Tobias  Vlćek <vlcek@beyondsimulations.com>
2024-08-22 16:24:46 +0200

    prepared final study

--- metro_framework.jl
+++ metro_framework.jl
@@ -23,22 +23,22 @@ include("metro_simulation.jl")
 include("metro_visuals.jl")
 
 # Parameters for the actual model
-set_safety = [0.9]                      # safety factor that limits the arc capacity
-set_max_enter = [120]                           # number of maximal entries per minute per station
-set_min_enter = [10]                              # min number of people allowed to enter
-set_scaling = [1.0]                                 # scaling of the metro queue (to test lower or higher demand)
-set_past_periods = [4]                      # timeframe to consider from the past during the optimization
-set_kind_opt = ["regularSqr"]                        # "regularSqr","linweight"
-set_kind_queue = ["lag_periods"]                    # "shift_periods","lag_periods"
+set_safety = [0.7,0.8,0.9,1.0]           # safety factor that limits the arc capacity
+set_max_enter = [120,240]                # number of maximal entries per minute per station
+set_min_enter = [0,10,20]                # min number of people allowed to enter
+set_scaling = [1.0]                      # scaling of the metro queue (to test lower or higher demand)
+set_past_periods = [1,2,3,4,5,6]         # timeframe to consider from the past during the optimization
+set_kind_opt = ["linweight"]             # "regularSqr","linweight"
+set_kind_queue = ["lag_periods"]         # "shift_periods","lag_periods"
 
 # Define static simulation data
-kind_sim = "bound"                  # "bound","inflow","unbound"
-minutes_in_period = 30              # minutes in each period (in 15 minute intervals!)
+kind_sim = "bound"                       # "bound","inflow","unbound"
+minutes_in_period = 15                   # minutes in each period (in 15 minute intervals!)
 
 # Define the start- and end time of the observed time horizon
 # Make sure that the horizon contains only one shift!
-start_time = DateTime("2022-11-29T05:00:00.00")
-end_time = DateTime("2022-11-30T04:59:00.00")
+start_time = DateTime("2022-11-27T05:00:00.00")
+end_time = DateTime("2022-11-28T04:59:00.00")
 
 struct MetroInstance
     kind_opt::String
